5656b4a66ddaa22e58029401166cd06e551c5085
johnrameshaws <127051725+johnrameshaws@users.noreply.github.com>
2025-03-19 22:45:19 -0500

    Update YahooFinance.py

--- YahooFinance.py
+++ YahooFinance.py
@@ -4,9 +4,13 @@ import numpy as np
 from datetime import datetime
 
 class TickerInfo:
-    def __init__(self, ticker_symbol):
+    def __init__(self, ticker_symbol, purchased_date, quantity, avg_cost_basis, country):
         self.ticker = yf.Ticker(ticker_symbol)
-    
+        self.purchased_date = purchased_date
+        self.quantity = quantity
+        self.avg_cost_basis = avg_cost_basis
+        self.country = country
+
     def get_full_info(self):
         info = {
             'company_info': self.ticker.info,
@@ -23,18 +27,82 @@ class TickerInfo:
         recommendations = self.ticker.recommendations.iloc[0].to_dict()
         return recommendations
 
+    def get_process_last_2years_performance(self):
+        period_perf_df=pd.DataFrame(columns=['period', 'quantity', 'actual_price', 'actual_cost', 'date', 'close', 'current_cost'])
+        start_date=self.purchased_date
+        print(start_date)
+        two_years_ago = datetime.now() - pd.DateOffset(years=2)
+        if two_years_ago < start_date:
+            start_date = two_years_ago
+
+        data = self.ticker.history(start=start_date)
+        """get min data"""
+        row={
+                'period': 'min',
+                'quantity': self.quantity,
+                'actual_price': self.avg_cost_basis,
+                'actual_cost': self.quantity * self.avg_cost_basis,
+                'date' : data['Close'].idxmin(),
+                'close': data['Close'].min(),
+                'current_cost': self.quantity * data['Close'].min(),
+            }
+        period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
+
+        """get max data"""
+        row={
+                'period': 'max',
+                'quantity': self.quantity,
+                'actual_price': self.avg_cost_basis,
+                'actual_cost': self.quantity * self.avg_cost_basis,
+                'date' : data['Close'].idxmax(),
+                'close': data['Close'].max(),
+                'current_cost': self.quantity * data['Close'].max(),
+            }
+        period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
+
+        """get current data"""
+        row={
+                'period': 'now',
+                'quantity': self.quantity,
+                'actual_price': self.avg_cost_basis,
+                'actual_cost': self.quantity * self.avg_cost_basis,
+                'date' : data.iloc[-1].name,
+                'close': data['Close'].iloc[-1],
+                'current_cost': self.quantity * data['Close'].iloc[-1],
+            }
+        period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
+
+        """get previous day data"""
+        row={
+                'period': 'previous day',
+                'quantity': self.quantity,
+                'actual_price': self.avg_cost_basis,
+                'actual_cost': self.quantity * self.avg_cost_basis,
+                'date' : data.iloc[-2].name,
+                'close': data['Close'].iloc[-2],
+                'current_cost': self.quantity * data['Close'].iloc[-2],
+            }
+        period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
+
+
     def get_last_2years_performance(self, quantity, avg_cost_basis, purchased_date):
         period_perf_df=pd.DataFrame(columns=['period', 'quantity', 'actual_price', 'actual_cost', 'date', 'close', 'current_cost'])
 
-        data = self.ticker.history(start=purchased_date)
+        start_date=self.purchased_date        
+        two_years_ago = datetime.now() - pd.DateOffset(years=2)
+        if two_years_ago < start_date:
+            start_date = two_years_ago
+
+        self.data = self.ticker.history(start=start_date)
+        #data = self.ticker.history(start=purchased_date)
         row={
                 'period': 'min',
                 'quantity': quantity,
                 'actual_price': avg_cost_basis,
                 'actual_cost': quantity * avg_cost_basis,
-                'date' : data['Close'].idxmin(),
-                'close': data['Close'].min(),
-                'current_cost': quantity * data['Close'].min(),
+                'date' : self.data['Close'].idxmin(),
+                'close': self.data['Close'].min(),
+                'current_cost': quantity * self.data['Close'].min(),
             }
         period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
 
@@ -43,34 +111,20 @@ class TickerInfo:
                 'quantity': quantity,
                 'actual_price': avg_cost_basis,
                 'actual_cost': quantity * avg_cost_basis,
-                'date' : data['Close'].idxmax(),
-                'close': data['Close'].max(),
-                'current_cost': quantity * data['Close'].max(),
+                'date' : self.data['Close'].idxmax(),
+                'close': self.data['Close'].max(),
+                'current_cost': quantity * self.data['Close'].max(),
             }
         period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
-
-        data = self.ticker.history(period='1wk', interval='1d')
-        _="""
-        row={
-                'period': 'now',
-                'quantity': quantity,
-                'actual_price': avg_cost_basis,
-                'actual_cost': quantity * avg_cost_basis,
-                'date' : datetime.timestamp(datetime.today()),
-                'close': self.get_current_price(),
-                'current_cost': quantity * self.get_current_price(),
-            }
-        period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
-        """
 
         row={
                 'period': 'now',
                 'quantity': quantity,
                 'actual_price': avg_cost_basis,
                 'actual_cost': quantity * avg_cost_basis,
-                'date' : data.iloc[-1].name,
-                'close': data['Close'].iloc[-1],
-                'current_cost': quantity * data['Close'].iloc[-1],
+                'date' : self.data.iloc[-1].name,
+                'close': self.data['Close'].iloc[-1],
+                'current_cost': quantity * self.data['Close'].iloc[-1],
             }
         period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
 
@@ -79,9 +133,9 @@ class TickerInfo:
                 'quantity': quantity,
                 'actual_price': avg_cost_basis,
                 'actual_cost': quantity * avg_cost_basis,
-                'date' : data.iloc[-2].name,
-                'close': data['Close'].iloc[-2],
-                'current_cost': quantity * data['Close'].iloc[-2],
+                'date' : self.data.iloc[-2].name,
+                'close': self.data['Close'].iloc[-2],
+                'current_cost': quantity * self.data['Close'].iloc[-2],
             }
         period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
 
@@ -90,61 +144,63 @@ class TickerInfo:
                 'quantity': quantity,
                 'actual_price': avg_cost_basis,
                 'actual_cost': quantity * avg_cost_basis,
-                'date' : data.iloc[0].name,
-                'close': data['Close'].iloc[0],
-                'current_cost': quantity * data['Close'].iloc[0],
+                'date' : self.data.iloc[-5].name,
+                'close': self.data['Close'].iloc[-5],
+                'current_cost': quantity * self.data['Close'].iloc[-5],
             }
         period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
 
-        data = self.ticker.history(period='1mo', interval='1d')    
+        #data = self.ticker.history(period='1mo', interval='1d')    
         row={
                 'period': '1mo before',
                 'quantity': quantity,
                 'actual_price': avg_cost_basis,
                 'actual_cost': quantity * avg_cost_basis,
-                'date' : data.iloc[0].name,
-                'close': data['Close'].iloc[0],
-                'current_cost': quantity * data['Close'].iloc[0],
+                'date' : self.data.iloc[-20].name,
+                'close': self.data['Close'].iloc[-20],
+                'current_cost': quantity * self.data['Close'].iloc[-20],
             }
         period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
 
-        data = self.ticker.history(period='6mo', interval='1d')    
+        #data = self.ticker.history(period='6mo', interval='1d')    
         row={
                 'period': '6mo before',
                 'quantity': quantity,
                 'actual_price': avg_cost_basis,
                 'actual_cost': quantity * avg_cost_basis,
-                'date' : data.iloc[0].name,
-                'close': data['Close'].iloc[0],
-                'current_cost': quantity * data['Close'].iloc[0],
+                'date' : self.data.iloc[-120].name,
+                'close': self.data['Close'].iloc[-120],
+                'current_cost': quantity * self.data['Close'].iloc[-120],
             }
         period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
 
-        data = self.ticker.history(period='1y', interval='1d')    
+        #data = self.ticker.history(period='1y', interval='1d')    
         row={
                 'period': '1yr before',
                 'quantity': quantity,
                 'actual_price': avg_cost_basis,
                 'actual_cost': quantity * avg_cost_basis,
-                'date' : data.iloc[0].name,
-                'close': data['Close'].iloc[0],
-                'current_cost': quantity * data['Close'].iloc[0],
+                'date' : self.data.iloc[-240].name,
+                'close': self.data['Close'].iloc[-240],
+                'current_cost': quantity * self.data['Close'].iloc[-240],
             }
         period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
-        self.yr1_min_price = data['Close'].min()
-        self.yr1_max_price = data['Close'].max()
 
-        data = self.ticker.history(period='2y', interval='1d')    
+        #data = self.ticker.history(period='2y', interval='1d')    
         row={
                 'period': '2yr before',
                 'quantity': quantity,
                 'actual_price': avg_cost_basis,
                 'actual_cost': quantity * avg_cost_basis,
-                'date' : data.iloc[0].name,
-                'close': data['Close'].iloc[0],
-                'current_cost': quantity * data['Close'].iloc[0],
+                'date' : self.data.iloc[0].name,
+                'close': self.data['Close'].iloc[0],
+                'current_cost': quantity * self.data['Close'].iloc[0],
             }
         period_perf_df=pd.concat([period_perf_df, pd.DataFrame([row])], ignore_index=True)
+
+        yr1_df=self.data[self.data.index.year >= datetime.now().year - 1]
+        self.yr1_min_price = yr1_df['Close'].min()
+        self.yr1_max_price = yr1_df['Close'].max()
 
         return period_perf_df.sort_values(by='date')
 
